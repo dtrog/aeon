f40b87227675b37873e838dd08596aad64e7e428
TheCharlatan <seb.kung@gmail.com>
2018-10-08 15:53:22 +0200

    Prepare Depends Packages for Gitian Scripts monero/#4526
    The gitian environment does not treat whitespaces in configure lines,
    like most other systems. The solution is to just remove them.

--- contrib/depends/packages/sodium.mk
+++ contrib/depends/packages/sodium.mk
@@ -3,6 +3,7 @@ $(package)_version=1.0.18
 $(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=libsodium-$($(package)_version).tar.gz
 $(package)_sha256_hash=6f504490b342a4f8a4c4a02fc9b866cbef8622d5df4e5452b46be121e46636c1
+$(package)_patches=fix-whitespace.patch
 
 define $(package)_set_vars
 $(package)_config_opts=--enable-static --disable-shared
@@ -11,6 +12,7 @@ endef
 
 define $(package)_config_cmds
   ./autogen.sh &&\
+  patch -p1 < $($(package)_patch_dir)/fix-whitespace.patch &&\
   $($(package)_autoconf) $($(package)_config_opts)
 endef
 
